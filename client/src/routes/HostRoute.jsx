0184e80417321ad5022dc18b88c594e768ed8b82
mdferdausalam19 <mdferdausalam333@gmail.com>
2025-09-10 20:21:01 +0600

    feat(client): add protected host route for host-specific access

--- client/src/routes/HostRoute.jsx
+++ client/src/routes/HostRoute.jsx
@@ -2,14 +2,14 @@ import useRole from "../hooks/useRole";
 import LoadingSpinner from "../components/shared/LoadingSpinner";
 import { Navigate } from "react-router";
 
-export default function AdminRoute({ children }) {
+export default function HostRoute({ children }) {
   const { role, isLoading } = useRole();
 
   if (isLoading) {
     return <LoadingSpinner />;
   }
 
-  if (role === "Admin") {
+  if (role === "Host") {
     return children;
   }
 
